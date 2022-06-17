583be65686844caf77369d2049e324eb123c6980
Vishnu KN <vishnunagaraj7@gmail.com>
2022-06-17 18:27:02 +0530

    Working code

---
+++ src/pages/MyNode.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MyNode(props) {
+  return (
+    <>
+      <h1>asdf</h1>
+      <g></g>
+    </>
+  );
+}
+
+export default MyNode;
--- src/pages/TreeViewComponent.js
+++ src/pages/TreeViewComponent.js
@@ -1,136 +1,9 @@
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-function TreeViewComponent() {
-  var pubs = {
-    name: "AUT-1",
-    children: [
-      {
-        name: "PUB-1",
-        children: [
-          {
-            name: "AUT-11",
-            children: [{ name: "AFF-111" }, { name: "AFF-112" }],
-          },
-          { name: "AUT-12", children: [{ name: "AFF-121" }] },
-          {
-            name: "AUT-13",
-            children: [{ name: "AFF-131" }, { name: "AFF-132" }],
-          },
-          { name: "AUT-14", children: [{ name: "AFF-141" }] },
-        ],
-      },
-      {
-        name: "PUB-2",
-        children: [
-          { name: "AUT-21" },
-          { name: "AUT-22" },
-          { name: "AUT-23" },
-          { name: "AUT-24" },
-          { name: "AUT-25" },
-          { name: "AUT-26" },
-          { name: "AUT-27" },
-          {
-            name: "AUT-28",
-            children: [
-              { name: "AFF-281" },
-              { name: "AFF-282" },
-              { name: "AFF-283" },
-              { name: "AFF-284" },
-              { name: "AFF-285" },
-              { name: "AFF-286" },
-            ],
-          },
-        ],
-      },
-      { name: "PUB-3" },
-      {
-        name: "PUB-4",
-        children: [
-          { name: "AUT-41" },
-          { name: "AUT-42" },
-          {
-            name: "AUT-43",
-            children: [
-              { name: "AFF-431" },
-              { name: "AFF-432" },
-              { name: "AFF-433" },
-              {
-                name: "AFF-434",
-                children: [{ name: "ADD-4341" }, { name: "ADD-4342" }],
-              },
-            ],
-          },
-          { name: "AUT-44" },
-        ],
-      },
-      {
-        name: "PUB-5",
-        children: [
-          {
-            name: "AUT-51",
-            children: [
-              { name: "AFF-511" },
-              { name: "AFF-512" },
-              { name: "AFF-513" },
-              { name: "AFF-514" },
-              { name: "AFF-515" },
-              { name: "AFF-516" },
-            ],
-          },
-          { name: "AUT-52" },
-          { name: "AUT-53" },
-          { name: "AUT-54" },
-          {
-            name: "AUT-55",
-            children: [
-              { name: "AFF-551" },
-              { name: "AFF-552" },
-              { name: "AFF-553" },
-              { name: "AFF-554" },
-            ],
-          },
-          { name: "AUT-56" },
-          { name: "AUT-57" },
-          { name: "AUT-58" },
-          { name: "AUT-59" },
-          { name: "AUT-591" },
-          { name: "AUT-592" },
-          { name: "AUT-593" },
-          { name: "AUT-594" },
-          { name: "AUT-595" },
-          { name: "AUT-596" },
-        ],
-      },
-      {
-        name: "PUB-6",
-        children: [
-          {
-            name: "AUT-61",
-            children: [
-              { name: "AFF-611" },
-              { name: "AFF-612" },
-              { name: "AFF-613" },
-              {
-                name: "AFF-614",
-                children: [{ name: "ADD-6141" }, { name: "ADD-6142" }],
-              },
-            ],
-          },
-          { name: "AUT-62" },
-          { name: "AUT-63" },
-          { name: "AUT-64" },
-          { name: "AUT-65" },
-          { name: "AUT-66" },
-          { name: "AUT-67" },
-          { name: "AUT-68" },
-          { name: "AUT-69" },
-        ],
-      },
-    ],
-  };
-
-  var diameter = 800;
+function TreeViewComponent(props) {
+  var pubs = props.pubs;
+  var diameter = props.diameter;
 
   var margin = { top: 20, right: 120, bottom: 20, left: 120 },
     width = diameter,
@@ -155,7 +28,7 @@ function TreeViewComponent() {
 
   root = useRef(pubs);
   const TreeViewContainer = useRef();
-  //
+
   useEffect(() => {
     if (TreeViewContainer.current.innerHTML === "") {
       var svg = d3
@@ -173,7 +46,7 @@ function TreeViewComponent() {
       root.current.x0 = height / 2;
       root.current.y0 = 0;
 
-      //root.children.forEach(collapse); // start with all children collapsed
+      //This function is populating everything inside the main g element
       update(root.current);
 
       d3.select(window.self.frameElement).style("height", "800px");
@@ -182,6 +55,9 @@ function TreeViewComponent() {
         // Compute the new tree layout.
         var nodes = tree.nodes(root.current),
           links = tree.links(nodes);
+
+        console.log("nodes", nodes);
+        console.log("links", links);
 
         // Normalize for fixed-depth.
         nodes.forEach(function (d) {
@@ -192,6 +68,7 @@ function TreeViewComponent() {
         var node = svg.selectAll("g.node").data(nodes, function (d) {
           return d.id || (d.id = ++i);
         });
+        console.log("node", node);
 
         // Enter any new nodes at the parent's previous position.
         var nodeEnter = node
